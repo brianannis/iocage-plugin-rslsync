585861db212c3b2b326d0f675a6b68acf2d0d567
Brian Annis <brianannis@users.noreply.github.com>
2020-10-27 17:22:09 -0400

    init

---
+++ post_install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+## add user to match up with rslsync guide
+pw user add rslsync -c rslsync -u 817 -d /nonexistent -s /usr/bin/nologin
+
+## create rslsync db folder
+mkdir -p /var/db/rslsync
+
+## install bin
+fetch -o /tmp https://download-cdn.resilio.com/stable/FreeBSD-x64/resilio-sync_freebsd_x64.tar.gz
+tar xzf /tmp/resilio-sync_freebsd_x64.tar.gz -C /usr/local/bin/
+rm /tmp/resilio-sync_freebsd_x64.tar.gz
+
+# enable the service
+sysrc -f /etc/rc.conf rslsync_enable="YES"
+
+# start the service
+service rslsync start 2>/dev/null
