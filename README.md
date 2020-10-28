47f56f4acdb06ca1ff77167e8cc7672d18fb2c01
Brian Annis <brianannis@users.noreply.github.com>
2020-10-28 17:07:32 -0400

    fix install script

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # iocage-plugin-rslsync
+
+## Setup guide
+https://www.resilio.com/blog/sync-hacks-how-to-set-up-freenas-with-bittorrent-sync-using-a-plugin
+
+## Install manually from internet
+
+```
+fetch jsonlink
+iocage fetch -P rslsync.json -n rslsync-plugin dhcp=on
+```
--- post_install.sh
+++ post_install.sh
@@ -3,19 +3,18 @@
 ## add user to match up with rslsync guide
 pw user add rslsync -c rslsync -u 817 -d /nonexistent -s /usr/bin/nologin
 
-## create rslsync db and bin folder
+## create rslsync db folder
 mkdir -p /var/db/rslsync
-chown rslsync:rslsync /var/db/rslsync
 mkdir -p /usr/local/bin
+chown rslsync:rslsync /var/db/rslsync
 
 ## install bin
 fetch -o /tmp https://download-cdn.resilio.com/stable/FreeBSD-x64/resilio-sync_freebsd_x64.tar.gz
-tar xvzf /tmp/resilio-sync_freebsd_x64.tar.gz
-cp rslsync /usr/local/bin
-rm /tmp/resilio-sync_freebsd_x64.tar.gz
+tar xvf /tmp/resilio-sync_freebsd_x64.tar.gz -C /usr/local/bin/
+rm /usr/local/bin/LICENSE.TXT /tmp/resilio-sync_freebsd_x64.tar.gz
 
 # enable the service
-sysrc -f /etc/rc.conf rslsync_enable="YES"
+sysrc rslsync_enable="YES"
 
 # start the service
 service rslsync start 2>/dev/null
